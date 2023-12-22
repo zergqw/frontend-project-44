8f760d5c9a267600f56845c79601f6d496ec3a9c
zergqw <eremenko.denis-qw@yandex.ru>
2023-12-22 06:48:03 +0300

    step9

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ https://asciinema.org/a/lTAYYxmG5J26qJiY1zwAdpXlS
 https://asciinema.org/a/W2WJEGwfqhOsoo5XPis63qrie
 https://asciinema.org/a/NvUscRIGNIioALlTwH6u2BG9W
 https://asciinema.org/a/mHeuTe8igf1c35kazVSASojeS
+https://asciinema.org/a/BM9tCNinFe6MWnXc4wg8EKblS
--- src/cli.js
+++ src/cli.js
@@ -3,12 +3,13 @@ import gameEven from '../bin/brain-even.js';
 import gameCalc from '../bin/brain-calc.js';
 import gameGCD from '../bin/brain-gcd.js';
 import gameProgression from '../bin/brain-progression.js';
+import gamePrime from '../bin/brain-prime.js';
 
 const sayName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log('Hello', name);
   const game = readlineSync.question(
-    'What kind of game do you want?\n 1 - Even or odd\n 2 - Caculator\n 3 - Greatest common divisor\n 4 - Arithmetic progression\n Your choose: ',
+    'What kind of game do you want?\n 1 - Even or odd\n 2 - Caculator\n 3 - Greatest common divisor\n 4 - Arithmetic progression\n 5 - Is it a prime number\n Your choose: ',
   );
   switch (game) {
     case '1':
@@ -22,6 +23,9 @@ const sayName = () => {
       break;
     case '4':
       gameProgression(name);
+      break;
+    case '5':
+      gamePrime(name);
       break;
     default:
       console.log('ERROR');
